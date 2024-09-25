5890241eba11c0494b0fcc78bdb078814f4b9a08
Angela Hu <angelahu925@gmail.com>
2024-09-24 23:01:57 -0700

    Added hack textbook pages from website

---
+++ theme.config.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from 'react';
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+import Logo from './public/Logo';
+
+const config: DocsThemeConfig = {
+  logo: <Logo />,
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+
+    const url = `https://hacktextbook.acmucsd.com${
+      defaultLocale === locale ? asPath : `/${locale}${asPath}`
+    }`;
+    const title = frontMatter.title || 'ACM Hack at UC San Diego Textbook';
+    const description =
+      frontMatter.description ||
+      "ACM Hack at UC San Diego is a community focused on software engineering and exploring what's possible through code!";
+
+    return (
+      <>
+        {/* google indexing data */}
+
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        {/* link sharing data */}
+
+        {/* page url to be used as permanent id */}
+        <meta property="og:url" content={url} />
+        {/* type of content */}
+        <meta property="og:type" content="website" />
+        {/* actual website title */}
+        <meta property="og:site_name" content="ACM Hack at UC San Diego Textbook" />
+        {/* title to display for the specific link being shared */}
+        <meta property="og:title" content={title} />
+        {/* preview description text */}
+        <meta property="og:description" content={description} />
+      </>
+    );
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – ACM Hack',
+      };
+    }
+    return {
+      titleTemplate: '%s',
+    };
+  },
+  footer: {
+    text: 'Made with 🧡 by ACM Hack!',
+  },
+  search: {
+    placeholder: 'Search',
+  },
+  sidebar: {
+    toggleButton: true,
+  },
+  docsRepositoryBase: 'https://github.com/acmucsd/hack-textbook/blob/main',
+};
+
+export default config;
